58727f9345fb25b7b6fb7b87e9bb7bb87e51036a
Natnael Yeshiwas <natiyeshi16@gmail.com>
2025-07-15 17:26:50 +0300

    s

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,12 +1,12 @@
 import Link from 'next/link';
-import logo from '@/../public/assets/logo/log.svg';
+import logo from '@/../public/assets/logo/icon.svg';
 import Image from 'next/image';
 
 export default function NotFound() {
   return (
     <div style={{ minHeight: '70vh', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', textAlign: 'center', padding: '2rem' }}>
-      <div style={{ display: 'flex', justifyContent: 'center' }}>
-        <Image src={logo} alt="Geneve Logo" width={200} height={200} style={{ marginBottom: '2rem' }} />
+      <div  style={{ display: 'flex', justifyContent: 'center' }}>
+        <Image src={logo} alt="Geneve Logo" width={150} height={150} style={{ marginBottom: '2rem' }} />
       </div>
       <h1 style={{ fontSize: '3rem', marginBottom: '1rem', color: '#d32f2f' }}>404</h1>
       <h2 style={{ fontSize: '2rem', marginBottom: '1rem' }}>Page Not Found</h2>
